70d982cddc555b3ebca074fa343d9a97e8c48726
kjy <kim0607mi@smail.kongju.ac.kr>
2026-04-17 19:34:02 +0900

    BOJ:10253

---
+++ 260417/10253.py
@@ -0,0 +1,15 @@
+# 백준 10253번
+
+from fractions import Fraction
+
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    
+    while(a!=1):
+        c=b//a+1
+        a=a*c-b
+        b=b*c
+        a,b=map(int,str(Fraction(a,b)).split('/'))
+    print(b)
+
+
